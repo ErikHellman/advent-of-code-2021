ec6f3b99bbd8e5673ae9e2de1faf9eaa17fd8637
Erik Hellman <erik.hellman@iteam.se>
2021-12-01 08:49:36 +0100

    Day 1 - Part 2

--- AoC_Day1/src/main/kotlin/Main.kt
+++ AoC_Day1/src/main/kotlin/Main.kt
@@ -4,17 +4,16 @@ fun main(args: Array<String>) {
     val reader = FileReader("scans.txt")
     var prev = -1
     var increases = 0
-    reader.readLines()
-        .map { it.toInt() }
-        .forEach {
-            prev = if (prev != -1) {
-                if (it > prev) {
-                    increases++
-                }
-                it
-            } else {
-                it
-            }
+    val inputs = reader.readLines().map { it.toInt() }
+    for (index in 0..inputs.size - 3) {
+        val first = inputs[index]
+        val second = inputs[index + 1]
+        val third = inputs[index + 2]
+        val sum = first + second + third
+        if (prev != -1 && sum > prev) {
+            increases++
         }
+        prev = sum
+    }
     println("Increases: $increases")
 }
