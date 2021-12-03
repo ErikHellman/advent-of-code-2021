434dfee2afec112cb9edc085832f3458e6fc35a5
Erik Hellman <erik.hellman@iteam.se>
2021-12-03 18:37:28 +0100

    Day 3 - Part 2

--- day3/src/main/kotlin/day3/Main.kt
+++ day3/src/main/kotlin/day3/Main.kt
@@ -3,32 +3,46 @@ package day3
 import java.io.FileReader
 
 fun main(args: Array<String>) {
-    val gammaArr = FileReader("input.txt")
-        .readLines()
-        .map {
-            it.split("")
-                .filter { c -> c.isNotBlank() }
-                .map { n -> n.toInt() }
+    val bitStrings = FileReader("input.txt").readLines()
+    val bitStringLength = bitStrings[0].length
+
+    val ogr = filterBitString(bitStringLength, true, bitStrings).joinToString("").toInt(2)
+    println("OGR $ogr")
+
+    val cos = filterBitString(bitStringLength, false, bitStrings).joinToString("").toInt(2)
+    println("COS $cos")
+
+    println("LSR ${cos * ogr}")
+}
+
+private fun filterBitString(
+    bitStringLength: Int,
+    mostCommon: Boolean,
+    candidates: List<String>
+): List<String> {
+    var result = candidates
+    repeat(bitStringLength) { position ->
+        val bitCriteria =
+            result.map {
+                it[position].digitToInt() }.fold(0) { acc, i ->
+                return@fold acc + if (i == 1) 1 else -1
+            }
+        result = result.filter {
+            if (mostCommon) {
+                if (bitCriteria >= 0) {
+                    it[position] == '1'
+                } else {
+                    it[position] == '0'
+                }
+            } else {
+                if (bitCriteria < 0) {
+                    it[position] == '1'
+                } else {
+                    it[position] == '0'
+                }
+            }
         }
-//        .onEach { println("Input: $it") }
-        .flatten()
-        .foldIndexed(mutableListOf(0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0)) { index, acc, i ->
-            val step = if (i == 1) 1 else -1
-            val bitPos = index % 12
-            acc[bitPos] += step
-            acc
-        }
-        .map { if (it >= 0) 1 else 0 }
-
-
-    val gammaString = gammaArr.joinToString(separator = "")
-    println("Gamma string: $gammaString")
-    val gamma = gammaString.toInt(2)
-    println("Gamma: $gamma")
-
-    val epsilon = gammaArr.map {
-        if (it == 0) 1 else 0
-    }.joinToString(separator = "").toInt(2)
-
-    println("Epsilon: $epsilon")
+        if (result.size == 1) return result
+    }
+    return result
 }
